887e6cf3166816d1d804a586e41ba2fb377e9f1b
Tommy Jepsen <tommy@tommyjepsen.com>
2024-04-09 09:12:28 +0200

    Updated SEO for the public page - fixed duplicated titles

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,10 @@ import { ThemeProvider } from "@/components/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "TWBlocks.com",
-  description:
-    "Website code blocks to copy/paste - based on ShadCN & Radix using Tailwind and NextJS",
+  title: "TWBlocks",
+  description: "Free SaaS website blocks based on React with shadcn & Tailwind",
+  keywords:
+    "tailwindcss, react, shadcn, design, webdesign, website, saas templates, saas website templates",
 };
 
 export default function RootLayout({
@@ -19,11 +20,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>TWBlocks - Free SaaS website blocks</title>
-        <meta
-          name="description"
-          content="Free SaaS website blocks based on React with shadcn & Tailwind"
-        />
         <meta
           property="og:title"
           content="TWBlocks - Free SaaS website blocks"
@@ -35,10 +31,6 @@ export default function RootLayout({
         <meta
           property="og:image"
           content="https://raw.githubusercontent.com/tommyjepsen/twblocks/main/public/hero4.png?raw=true"
-        />
-        <meta
-          name="keywords"
-          content="tailwindcss, react, shadcn, design, webdesign, website, saas templates, saas website templates"
         />
       </head>
       <body className={inter.className}>
