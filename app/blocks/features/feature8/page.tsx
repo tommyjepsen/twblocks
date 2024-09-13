9b380b9bc81dae88009c7fc379b3d99d8c6cab29
Tommy Jepsen <tommy@tommyjepsen.com>
2024-09-13 09:58:29 +0200

    Added feature 8 to the website

---
+++ app/blocks/features/feature8/page.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+import { Feature8 } from "@/blocks/feature/feature8";
+import { CopyCodeButton } from "@/components/copy-code-button";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Terminal } from "lucide-react";
+import Link from "next/link";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+export default function Block() {
+  const code = `import { Badge } from "@/components/ui/badge";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+export const Feature8 = () => (
+  <div className="w-full py-20 lg:py-40">
+    <div className="container mx-auto">
+      <div className="grid grid-cols-1 lg:grid-cols-2 justify-end items-end  gap-10">
+        <div className="flex gap-4 flex-col items-start">
+          <div>
+            <Badge>Platform</Badge>
+          </div>
+          <div className="flex gap-2 flex-col">
+            <h2 className="text-xl md:text-3xl lg:text-5xl tracking-tighter lg:max-w-xl font-regular text-left">
+              This is the start of something new
+            </h2>
+            <p className="text-lg  max-w-xl lg:max-w-sm leading-relaxed tracking-tight text-muted-foreground  text-left">
+              Managing a small business today is already tough. Avoid further
+              complications by ditching outdated, tedious trade methods. Our
+              goal is to streamline SMB trade, making it easier and faster than
+              ever.
+            </p>
+          </div>
+        </div>
+        <div className="w-full max-w-full px-6">
+          <Carousel>
+            <CarouselContent>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem key={index}>
+                  <div className="flex rounded-md aspect-video bg-muted items-center justify-center p-6">
+                    <span className="text-sm">
+                      Platform Screenshot {index + 1}
+                    </span>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </div>
+      </div>
+    </div>
+  </div>
+);`;
+
+  return (
+    <div className="min-h-screen w-full flex flex-col">
+      <div className="bg-secondary mt-20 w-full">
+        <div className="container items-center text-secondary-foreground text-sm text-center py-4 flex flex-row justify-between">
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <Link href="/">Blocks</Link>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <Link href="/#features">Features</Link>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Feature7</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
+      </div>
+      <div className="flex bg-background justify-center items-start flex-1">
+        <div className="w-full max-w-8xl relative">
+          <div className="absolute top-8 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-row gap-2">
+            <CopyCodeButton code={code} />
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button variant="outline" className="gap-2">
+                  <Terminal className="w-4 h-4" />
+                  Show code
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="max-w-[800px]">
+                <SyntaxHighlighter
+                  language="javascript"
+                  className="rounded-md max-h-[70vh]"
+                  style={vscDarkPlus}
+                >
+                  {code}
+                </SyntaxHighlighter>
+              </DialogContent>
+            </Dialog>
+          </div>
+          <div className="rounded-md bg-background">
+            <Feature8 />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- components/blocks/features.tsx
+++ components/blocks/features.tsx
@@ -122,6 +122,21 @@ export const Features = () => (
               </div>
             </div>
           </Link>
+          <Link href="blocks/features/feature8" className="hover:opacity-70">
+            <div className="flex flex-col h-full">
+              <div className="bg-muted rounded-md aspect-video p-8 lg:p-20 h-full">
+                <Image
+                  src="/feature8.png"
+                  alt="feature8"
+                  width={3600}
+                  height={1972}
+                  layout="responsive"
+                  className="rounded-md"
+                  loading="lazy"
+                />
+              </div>
+            </div>
+          </Link>
         </div>
       </div>
     </div>
