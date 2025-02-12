057a789382afeb94e10ec0d6359047250c4c7e38
CoinMarketJob <kucukmira@gmail.com>
2025-02-13 00:46:03 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -14,24 +14,16 @@ import { FAQs } from "@/components/blocks/faqs";
 import { Contacts } from "@/components/blocks/contacts";
 import { Footers } from "@/components/blocks/footers";
 import { Headers } from "@/components/blocks/headers";
+import { Footer, DayPickerProvider } from "react-day-picker";
 
 export default function Home() {
   return (
-    <>
-      <Header />
-      <Hero />
-      <Headers />
-      <Heroes />
-      <Cases />
-      <Testimonials />
-      <Features />
-      <Pricings />
-      <Stats />
-      <CTAs />
-      <Blogs />
-      <FAQs />
-      <Contacts />
-      <Footers />
-    </>
+    <DayPickerProvider initialProps={{}}>
+      <>
+        <Header />
+        <Hero />
+        <Footer />
+      </>
+    </DayPickerProvider>
   );
 }
